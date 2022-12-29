0dd5c2720d2595de99038308f6e06221c5c7398a
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2022-12-28 21:12:56 -0300

    Update index.js

--- ReactBasico/src/componentes/Time/index.js
+++ ReactBasico/src/componentes/Time/index.js
@@ -1,9 +1,9 @@
 import "./Time.css"
 import Colaborador from '../Colaborador'
-// import colaborador from "../Colaborador"
+
+// Componente de fundo de cada seção de times
 const Time = (props) => {
     return(
-
        (props.colaboradores.length > 0) ? <section className="time" style={{backgroundColor:props.corSecundaria}}>
             <h3 style={{borderColor:props.corPrimaria}}>{props.nome}</h3>
             <div className="colaboradores">{props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corDeFundo={props.corPrimaria} />)}</div>
